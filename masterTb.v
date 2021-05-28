74a9c24636348c5c3566e0f481484260516305f9
Aditya Jain <54070532+adz0612@users.noreply.github.com>
2021-05-28 21:31:03 +0530

    Update masterTb.v

--- masterTb.v
+++ masterTb.v
@@ -1,1 +1,34 @@
-// Tb added.
+module masterTb();
+reg clk;
+reg reset;
+wire done;
+
+initial begin
+    clk = 0;
+end
+
+initial begin
+    reset = 0; #40
+    reset = 1;
+end
+
+initial begin
+    forever #100 clk = ~clk;
+end
+/*
+ ram #() dut3 (
+    .clka(clk),
+     .reset(reset)
+     
+     
+     
+        );
+
+               */
+               ram M1(.clka(clk),.reset(reset) ,.done(done));
+               
+               
+               
+               
+endmodule
+
